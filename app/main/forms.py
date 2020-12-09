9d31f2c78d7b6b1fc7a3ce5e21d86a061ce2d316
umedith <umedith03@gmail.com>
2020-12-09 06:28:38 +0200

    added a function to subscribe

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,11 +7,10 @@ class CommentsForm(FlaskForm):
     comment = TextAreaField('Comment', validators=[Required()])
     # vote=RadioField('default field arguments', choices=[('1', 'UpVote'), ('1', 'DownVote')])
     submit = SubmitField('SUBMIT')
-    
+
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
-
 
 class BlogForm(FlaskForm):
     title = StringField('Enter title',validators = [Required()])
