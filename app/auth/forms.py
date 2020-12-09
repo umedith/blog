7a22f72f1b31831a6ace10d06d98f0227d4d162d
umedith <umedith03@gmail.com>
2020-12-09 06:26:19 +0200

    added the router method to link to different pages

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import Required, Email, EqualTo
 from ..models import User
-
 from wtforms import ValidationError
  
 class RegistrationForm(FlaskForm):
